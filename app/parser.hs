f31c9671beaa642949983c133b138c05d36ac270
Christina Haber <christina.haber@net.usj.edu.lb>
2024-12-10 20:34:14 +0200

    Implemented basic parser combinator structure using newtype Parser. Along with charP a fundamental parser combinator designed to parse a single character from the input string.

--- app/parser.hs
+++ app/parser.hs
@@ -25,3 +25,25 @@ data JSON
     | JArray [JSON]
     | JObject [(String, JSON)]                                  
   deriving (Show, Eq)
+
+
+--Implementation of a parser
+--the parser takes a string and return whaterver we want to parse
+
+-- Maybe is used in case the parser fails when the input does not conform the expected format
+--runParser is the fct that defines what the parser does
+newtype Parser a = Parser 
+    { runParser :: String -> Maybe(String ,a)
+    } 
+
+
+charP :: Char -> Parser Char
+charP x = Parser f
+    where
+        f (y:ys)
+            | y == x = Just (ys,x)
+            | otherwise = Nothing
+        f [] = Nothing 
+
+
+
