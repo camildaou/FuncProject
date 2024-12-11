ed9dad22c90de5fb369a0d76cad2fe1d5d9fa9ac
GeorgioZoughby <georgio.zoghby@outlook.com>
2024-12-12 00:49:48 +0200

    Changed the getNestedField to handle the jsonObjects inside an array value of a JSON object something like this employees: [{ id: E123, name: Alice Johnson, department: Marketing, position: Manager, hire_date: 2020-01-15, salary: 75000.0, active: True },...]

--- app/Utils.hs
+++ app/Utils.hs
@@ -4,10 +4,11 @@ module Utils where
 
 import Parser (JSON(..))
 import Data.List (find)
+import Data.Char (isDigit)
 
 -- | Function to get a field from a JObject
 getField :: String -> JSON -> Maybe JSON
-getField key (JObject p) = 
+getField key (JObject p) =
     case find (\(k, _) -> matchKey k) p of
         Just (_, v) -> Just v
         Nothing     -> Nothing
@@ -16,12 +17,26 @@ getField key (JObject p) =
     matchKey s = s == key
 getField _ _ = Nothing
 
--- | Function to access nested fields using dot notation
+
 getNestedField :: [String] -> JSON -> Maybe JSON
-getNestedField [] expr = Just expr
-getNestedField (k:ks) expr = do
-    field <- getField k expr
-    getNestedField ks field
+getNestedField [] json = Just json
+getNestedField (key:keys) (JObject obj) =
+    case lookup key obj of
+        Just value -> getNestedField keys value
+        Nothing -> Nothing
+getNestedField (key:keys) (JArray arr)
+    | all isDigit key =
+        let index = read key :: Int
+        in if index >= 0 && index < length arr
+            then getNestedField keys (arr !! index)
+            else Nothing
+    | otherwise =
+        let results = map (\item -> getNestedField (key:keys) item) arr
+        in Just (JArray (map (\(Just x) -> x) (filter (/= Nothing) results)))
+getNestedField _ _ = Nothing
+
+
+
 
 -- | Function to convert dot-separated field string to list
 parseFieldPath :: String -> [String]
